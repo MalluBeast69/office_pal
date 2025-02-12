89d87b5c877724ec4fc11ba9fdd85cd3fa3d74fd
MalluBeast69 <MalluBeast69@users.noreply.github.com>
2025-02-12 15:31:39 +0000

    deploy: b80ba4d8feabb97555d2bd23f8af541201bda06b

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"e672b006cb34c921db85b8e2f482ed3144a457
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1777540663"
+    serviceWorkerVersion: "4161060200"
   }
 });

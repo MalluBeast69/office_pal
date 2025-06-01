a20efa760188128f0ed4c161430bbcf27fc45bc2
Your Name <you@example.com>
2025-06-02 02:10:01 +0530

    Fix: Fixed some blunders 2

--- DB.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-REM Connect to Supabase database using psql
-psql "postgresql://postgres:broshameel316001@db.fnxirlpiqciezifongxb.supabase.co:5432/postgres"
--- lib/core/constants.dart
+++ lib/core/constants.dart
@@ -1,5 +1,5 @@
 class Constants {
-  static const String supabaseUrl = 'https://fnxirlpiqciezifongxb.supabase.co';
+  static const String supabaseUrl = String.fromEnvironment('SUPABASE_URL');
   static const String supabaseAnonKey =
-      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZueGlybHBpcWNpZXppZm9uZ3hiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzYzNjIxMzgsImV4cCI6MjA1MTkzODEzOH0.qbqwZJr9ufNbs3mjQHFuJMNlef-mUwBNoCaoeuHDGhM';
+      String.fromEnvironment('SUPABASE_ANON_KEY');
 }
